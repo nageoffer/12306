49446fee7d6c6b3ac38f2d2dac7e3752b2baa5ca
chen.ma <machen@apache.org>
2023-06-07 23:22:35 +0800

    feature: 基础架构-分页工具类开发转换重载方法

--- frameworks/database/src/main/java/org/opengoofy/index12306/framework/starter/database/toolkit/PageUtil.java
+++ frameworks/database/src/main/java/org/opengoofy/index12306/framework/starter/database/toolkit/PageUtil.java
@@ -23,6 +23,10 @@ import org.opengoofy.index12306.framework.starter.common.toolkit.BeanUtil;
 import org.opengoofy.index12306.framework.starter.convention.page.PageRequest;
 import org.opengoofy.index12306.framework.starter.convention.page.PageResponse;
 
+import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 /**
  * 分页工具类
  *
@@ -32,9 +36,6 @@ public class PageUtil {
 
     /**
      * {@link PageRequest} to {@link Page}
-     *
-     * @param pageRequest
-     * @return
      */
     public static Page convert(PageRequest pageRequest) {
         return convert(pageRequest.getCurrent(), pageRequest.getSize());
@@ -42,33 +43,20 @@ public class PageUtil {
 
     /**
      * {@link PageRequest} to {@link Page}
-     *
-     * @param current
-     * @param size
-     * @return
      */
     public static Page convert(long current, long size) {
         return new Page(current, size);
     }
 
     /**
-     * {@link IPage} to {@link PageRequest}
-     *
-     * @param iPage
-     * @return
+     * {@link IPage} to {@link PageResponse}
      */
     public static PageResponse convert(IPage iPage) {
         return buildConventionPage(iPage);
     }
 
     /**
-     * {@link IPage} to {@link PageRequest}
-     *
-     * @param iPage
-     * @param targetClass
-     * @param <TARGET>
-     * @param <ORIGINAL>
-     * @return
+     * {@link IPage} to {@link PageResponse}
      */
     public static <TARGET, ORIGINAL> PageResponse<TARGET> convert(IPage<ORIGINAL> iPage, Class<TARGET> targetClass) {
         iPage.convert(each -> BeanUtil.convert(each, targetClass));
@@ -76,12 +64,29 @@ public class PageUtil {
     }
 
     /**
-     * {@link IPage} build to {@link PageRequest}
-     *
-     * @param iPage
-     * @return
+     * {@link IPage} to {@link PageResponse}
+     */
+    public static <TARGET, ORIGINAL> PageResponse<TARGET> convert(IPage<ORIGINAL> iPage, Function<? super ORIGINAL, ? extends TARGET> mapper) {
+        List<TARGET> targetDataList = iPage.getRecords().stream()
+                .map(mapper)
+                .collect(Collectors.toList());
+        return PageResponse.<TARGET>builder()
+                .current(iPage.getCurrent())
+                .size(iPage.getSize())
+                .records(targetDataList)
+                .total(iPage.getTotal())
+                .build();
+    }
+
+    /**
+     * {@link IPage} build to {@link PageResponse}
      */
     private static PageResponse buildConventionPage(IPage iPage) {
-        return PageResponse.builder().current(iPage.getCurrent()).size(iPage.getSize()).records(iPage.getRecords()).total(iPage.getTotal()).build();
+        return PageResponse.builder()
+                .current(iPage.getCurrent())
+                .size(iPage.getSize())
+                .records(iPage.getRecords())
+                .total(iPage.getTotal())
+                .build();
     }
 }
