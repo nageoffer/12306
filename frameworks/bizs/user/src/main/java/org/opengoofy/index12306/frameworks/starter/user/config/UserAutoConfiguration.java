1f5db3d5af608e2a3b6506db3c8577188586a824
chen.ma <machen@apache.org>
2023-06-21 21:32:30 +0800

    feature: 开发基础业务之用户组件库

---
+++ frameworks/bizs/user/src/main/java/org/opengoofy/index12306/frameworks/starter/user/config/UserAutoConfiguration.java
@@ -0,0 +1,44 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.frameworks.starter.user.config;
+
+import org.opengoofy.index12306.frameworks.starter.user.core.UserTransmitFilter;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import static org.opengoofy.index12306.framework.starter.bases.constant.FilterOrderConstant.USER_TRANSMIT_FILTER_ORDER;
+
+/**
+ * 用户配置自动装配
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+public class UserAutoConfiguration {
+
+    /**
+     * 用户信息传递过滤器
+     */
+    @Bean
+    public FilterRegistrationBean<UserTransmitFilter> globalUserTransmitFilter() {
+        FilterRegistrationBean<UserTransmitFilter> registration = new FilterRegistrationBean<>();
+        registration.setFilter(new UserTransmitFilter());
+        registration.addUrlPatterns("/*");
+        registration.setOrder(USER_TRANSMIT_FILTER_ORDER);
+        return registration;
+    }
+}
---
+++ frameworks/bizs/user/src/main/java/org/opengoofy/index12306/frameworks/starter/user/core/UserTransmitFilter.java
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.frameworks.starter.user.core;
+
+import jakarta.servlet.Filter;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
+import org.opengoofy.index12306.framework.starter.bases.constant.UserConstant;
+import org.slf4j.MDC;
+import org.springframework.util.StringUtils;
+
+import java.io.IOException;
+import java.net.URLDecoder;
+
+/**
+ * 用户信息传输过滤器
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+public class UserTransmitFilter implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        String userId = httpServletRequest.getHeader(UserConstant.USER_ID_KEY);
+        if (StringUtils.hasText(userId)) {
+            String userName = httpServletRequest.getHeader(UserConstant.USER_NAME_KEY);
+            String realMame = httpServletRequest.getHeader(UserConstant.REAL_NAME_KEY);
+            if (StringUtils.hasText(userName)) {
+                userName = URLDecoder.decode(userName, "UTF-8");
+            }
+            if (StringUtils.hasText(realMame)) {
+                realMame = URLDecoder.decode(realMame, "UTF-8");
+            }
+            MDC.put(UserConstant.USER_ID_KEY, userId);
+            MDC.put(UserConstant.USER_NAME_KEY, userName);
+            MDC.put(UserConstant.REAL_NAME_KEY, realMame);
+        }
+        try {
+            filterChain.doFilter(servletRequest, servletResponse);
+        } finally {
+            MDC.clear();
+        }
+    }
+}
