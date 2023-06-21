9b02076df6f16dc378237801a3657c20883bd560
chen.ma <machen@apache.org>
2023-06-21 21:31:01 +0800

    feature: 开发网关服务相关功能接口

--- services/gateway-service/src/main/java/org/opengoofy/index12306/biz/gatewayservice/config/Config.java
+++ services/gateway-service/src/main/java/org/opengoofy/index12306/biz/gatewayservice/config/Config.java
@@ -15,15 +15,20 @@
  * limitations under the License.
  */
 
-package org.opengoofy.index12306.biz.gatewayservice;
+package org.opengoofy.index12306.biz.gatewayservice.config;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import lombok.Data;
 
-@SpringBootTest
-class GatewayServiceApplicationTests {
+/**
+ * 过滤器配置
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@Data
+public class Config {
 
-    @Test
-    void contextLoads() {
-    }
+    /**
+     * 黑名单路径
+     */
+    private String blackPathPre;
 }
---
+++ services/gateway-service/src/main/java/org/opengoofy/index12306/biz/gatewayservice/filter/TokenValidateGatewayFilterFactory.java
@@ -0,0 +1,74 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.biz.gatewayservice.filter;
+
+import org.opengoofy.index12306.biz.gatewayservice.config.Config;
+import org.opengoofy.index12306.framework.starter.bases.constant.UserConstant;
+import org.opengoofy.index12306.frameworks.starter.user.core.UserInfoDTO;
+import org.opengoofy.index12306.frameworks.starter.user.toolkit.JWTUtil;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+
+/**
+ * SpringCloud Gateway Token 拦截器
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@Component
+public class TokenValidateGatewayFilterFactory extends AbstractGatewayFilterFactory<Config> {
+
+    public TokenValidateGatewayFilterFactory() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return (exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            String requestPath = request.getPath().toString();
+            if (requiresTokenValidation(requestPath, config.getBlackPathPre())) {
+                String token = request.getHeaders().getFirst("Authorization");
+                UserInfoDTO userInfo;
+                if (!validateToken(token) || (userInfo = JWTUtil.parseJwtToken(token)) == null) {
+                    ServerHttpResponse response = exchange.getResponse();
+                    response.setStatusCode(HttpStatus.UNAUTHORIZED);
+                    return response.setComplete();
+                }
+                ServerHttpRequest.Builder builder = exchange.getRequest().mutate().headers(httpHeaders -> {
+                    httpHeaders.set(UserConstant.USER_ID_KEY, userInfo.getUserId());
+                    httpHeaders.set(UserConstant.USER_NAME_KEY, userInfo.getUsername());
+                    httpHeaders.set(UserConstant.REAL_NAME_KEY, userInfo.getRealName());
+                });
+                return chain.filter(exchange.mutate().request(builder.build()).build());
+            }
+            return chain.filter(exchange);
+        };
+    }
+
+    private boolean requiresTokenValidation(String requestPath, String blackPathPre) {
+        return requestPath.startsWith(blackPathPre);
+    }
+
+    private boolean validateToken(String token) {
+        return true;
+    }
+}
