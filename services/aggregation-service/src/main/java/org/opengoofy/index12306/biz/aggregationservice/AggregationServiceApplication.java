4e92b96554fc140ad20dcea1383c738119ab5a38
chen.ma <machen@apache.org>
2023-06-21 21:31:16 +0800

    feature: 开发聚合层服务相关功能接口

---
+++ services/aggregation-service/src/main/java/org/opengoofy/index12306/biz/aggregationservice/AggregationServiceApplication.java
@@ -0,0 +1,37 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.biz.aggregationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 12306 聚合服务应用启动器
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@SpringBootApplication(scanBasePackages = {
+        "org.opengoofy.index12306.biz.userservice",
+        "org.opengoofy.index12306.biz.ticketservice"
+})
+public class AggregationServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AggregationServiceApplication.class, args);
+    }
+}
