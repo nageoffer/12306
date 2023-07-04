fffcf4e703f07c021fe0f699577394e53c2ed513
chen.ma <machen@apache.org>
2023-07-04 22:08:46 +0800

    feature: 服务端异常自定义错误提示信息

--- frameworks/convention/src/main/java/org/opengoofy/index12306/framework/starter/convention/exception/ServiceException.java
+++ frameworks/convention/src/main/java/org/opengoofy/index12306/framework/starter/convention/exception/ServiceException.java
@@ -20,6 +20,8 @@ package org.opengoofy.index12306.framework.starter.convention.exception;
 import org.opengoofy.index12306.framework.starter.convention.errorcode.BaseErrorCode;
 import org.opengoofy.index12306.framework.starter.convention.errorcode.IErrorCode;
 
+import java.util.Optional;
+
 /**
  * 服务端异常
  *
@@ -40,7 +42,7 @@ public class ServiceException extends AbstractException {
     }
 
     public ServiceException(String message, Throwable throwable, IErrorCode errorCode) {
-        super(message, throwable, errorCode);
+        super(Optional.ofNullable(message).orElse(errorCode.message()), throwable, errorCode);
     }
 
     @Override
