32bc4102f388256b4afac097fae420f27d3b3d0d
chen.ma <machen@apache.org>
2023-07-08 13:18:12 +0800

    feature: 修改座位表主键 ID 类型

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/dao/entity/CarriageDO.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/dao/entity/CarriageDO.java
@@ -17,6 +17,8 @@
 
 package org.opengoofy.index12306.biz.ticketservice.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import org.opengoofy.index12306.framework.starter.database.base.BaseDO;
@@ -33,6 +35,7 @@ public class CarriageDO extends BaseDO {
     /**
      * id
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     /**
