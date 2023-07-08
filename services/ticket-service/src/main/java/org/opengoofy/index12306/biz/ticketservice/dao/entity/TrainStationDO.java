9a19264ea90ad6784d254088a8501128b8de1bda
chen.ma <machen@apache.org>
2023-07-08 15:25:24 +0800

    feature: 列车站点实体修改主键生成策略以及增加车站 ID

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/dao/entity/TrainStationDO.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/dao/entity/TrainStationDO.java
@@ -17,6 +17,8 @@
 
 package org.opengoofy.index12306.biz.ticketservice.dao.entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Data;
 import org.opengoofy.index12306.framework.starter.database.base.BaseDO;
@@ -35,12 +37,18 @@ public class TrainStationDO extends BaseDO {
     /**
      * id
      */
+    @TableId(type = IdType.AUTO)
     private Long id;
 
     /**
      * 车次id
      */
     private Long trainId;
+
+    /**
+     * 车站id
+     */
+    private Long stationId;
 
     /**
      * 站点顺序
