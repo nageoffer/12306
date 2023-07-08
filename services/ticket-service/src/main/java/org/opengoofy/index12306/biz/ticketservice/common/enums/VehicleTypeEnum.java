daae383444f6cebf76d2fd63941f9794c87d0d18
chen.ma <machen@apache.org>
2023-07-08 16:15:28 +0800

    feature: 补充交通工具类型对应的座位集合

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/common/enums/VehicleTypeEnum.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/common/enums/VehicleTypeEnum.java
@@ -17,11 +17,21 @@
 
 package org.opengoofy.index12306.biz.ticketservice.common.enums;
 
+import cn.hutool.core.collection.ListUtil;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
+
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.BUSINESS_CLASS;
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.FIRST_CLASS;
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.FIRST_SLEEPER;
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.NO_SEAT_SLEEPER;
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.SECOND_CLASS;
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.SECOND_CLASS_CABIN_SEAT;
+import static org.opengoofy.index12306.biz.ticketservice.common.enums.VehicleSeatTypeEnum.SECOND_SLEEPER;
 
 /**
  * 交通工具类型
@@ -34,28 +44,39 @@ public enum VehicleTypeEnum {
     /**
      * 高铁
      */
-    HIGH_SPEED_RAIN(0, "HIGH_SPEED_RAIN"),
+    HIGH_SPEED_RAIN(0, "HIGH_SPEED_RAIN", "高铁", ListUtil.of(BUSINESS_CLASS.getCode(), FIRST_CLASS.getCode(), SECOND_CLASS.getCode())),
 
     /**
-     * 火车
+     * 动车
      */
-    TRAIN(1, "TRAIN"),
+    BULLET(1, "BULLET", "动车", ListUtil.of(SECOND_CLASS_CABIN_SEAT.getCode(), FIRST_SLEEPER.getCode(), SECOND_SLEEPER.getCode(), NO_SEAT_SLEEPER.getCode())),
+
+    /**
+     * 普通车
+     */
+    REGULAR_TRAIN(2, "REGULAR_TRAIN", "普通车", null),
 
     /**
      * 汽车
      */
-    CAR(2, "CAR"),
+    CAR(3, "CAR", "汽车", null),
 
     /**
      * 飞机
      */
-    AIRPLANE(3, "AIRPLANE");
+    AIRPLANE(4, "AIRPLANE", "飞机", null);
 
     @Getter
     private final Integer code;
 
     @Getter
     private final String name;
+
+    @Getter
+    private final String value;
+
+    @Getter
+    private final List<Integer> seatTypes;
 
     /**
      * 根据编码查找名称
@@ -65,6 +86,17 @@ public enum VehicleTypeEnum {
                 .filter(each -> Objects.equals(each.getCode(), code))
                 .findFirst()
                 .map(VehicleTypeEnum::getName)
+                .orElse(null);
+    }
+
+    /**
+     * 根据编码查找座位类型集合
+     */
+    public static List<Integer> findSeatTypesByCode(Integer code) {
+        return Arrays.stream(VehicleTypeEnum.values())
+                .filter(each -> Objects.equals(each.getCode(), code))
+                .findFirst()
+                .map(VehicleTypeEnum::getSeatTypes)
                 .orElse(null);
     }
 }
