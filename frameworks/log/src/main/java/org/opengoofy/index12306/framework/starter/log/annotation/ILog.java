d35446f5ef3ceeb1bd883bafd6a5d5e6430dcb3a
chen.ma <machen@apache.org>
2023-07-11 22:15:04 +0800

    feature: 开发日志打印组件库

---
+++ frameworks/log/src/main/java/org/opengoofy/index12306/framework/starter/log/annotation/ILog.java
@@ -0,0 +1,48 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.framework.starter.log.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Log 注解打印，可以标记在类或者方法上
+ * 标记在类上，类下所有方法都会打印；标记在方法上，仅打印标记方法；如果类或者方法上都有标记，以方法上注解为准
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@Target({ElementType.TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ILog {
+
+    /**
+     * 入参打印
+     *
+     * @return 打印结果中是否包含入参，{@link Boolean#TRUE} 打印，{@link Boolean#FALSE} 不打印
+     */
+    boolean input() default true;
+
+    /**
+     * 出参打印
+     *
+     * @return 打印结果中是否包含出参，{@link Boolean#TRUE} 打印，{@link Boolean#FALSE} 不打印
+     */
+    boolean output() default true;
+}
---
+++ frameworks/log/src/main/java/org/opengoofy/index12306/framework/starter/log/core/ILogPrintAspect.java
@@ -0,0 +1,122 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.framework.starter.log.core;
+
+import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.date.SystemClock;
+import com.alibaba.fastjson2.JSON;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.Data;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.opengoofy.index12306.framework.starter.log.annotation.ILog;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.lang.reflect.Method;
+import java.util.Optional;
+
+/**
+ * {@link ILog} 日志打印 AOP 切面
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@Aspect
+public class ILogPrintAspect {
+
+    /**
+     * 打印类或方法上的 {@link ILog}
+     */
+    @Around("@within(org.opengoofy.index12306.framework.starter.log.annotation.ILog) || @annotation(org.opengoofy.index12306.framework.starter.log.annotation.ILog)")
+    public Object printMLog(ProceedingJoinPoint joinPoint) throws Throwable {
+        long startTime = SystemClock.now();
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        Logger log = LoggerFactory.getLogger(methodSignature.getDeclaringType());
+        String beginTime = DateUtil.now();
+        Object result = null;
+        try {
+            result = joinPoint.proceed();
+        } finally {
+            Method targetMethod = joinPoint.getTarget().getClass().getDeclaredMethod(methodSignature.getName(), methodSignature.getMethod().getParameterTypes());
+            ILog logAnnotation = Optional.ofNullable(targetMethod.getAnnotation(ILog.class)).orElse(joinPoint.getTarget().getClass().getAnnotation(ILog.class));
+            if (logAnnotation != null) {
+                ILogPrint logPrint = new ILogPrint();
+                logPrint.setBeginTime(beginTime);
+                if (logAnnotation.input()) {
+                    logPrint.setInputParams(buildInput(joinPoint));
+                }
+                if (logAnnotation.output()) {
+                    logPrint.setOutputParams(result);
+                }
+                String methodType = "", requestURI = "";
+                try {
+                    ServletRequestAttributes servletRequestAttributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+                    assert servletRequestAttributes != null;
+                    methodType = servletRequestAttributes.getRequest().getMethod();
+                    requestURI = servletRequestAttributes.getRequest().getRequestURI();
+                } catch (Exception ignored) {
+                }
+                log.info("[{}] {}, executeTime: {}ms, info: {}", methodType, requestURI, SystemClock.now() - startTime, JSON.toJSONString(logPrint));
+            }
+        }
+        return result;
+    }
+
+    private Object[] buildInput(ProceedingJoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        Object[] printArgs = new Object[args.length];
+        for (int i = 0; i < args.length; i++) {
+            if ((args[i] instanceof HttpServletRequest) || args[i] instanceof HttpServletResponse) {
+                continue;
+            }
+            if (args[i] instanceof byte[]) {
+                printArgs[i] = "byte array";
+            } else if (args[i] instanceof MultipartFile) {
+                printArgs[i] = "file";
+            } else {
+                printArgs[i] = args[i];
+            }
+        }
+        return printArgs;
+    }
+
+    @Data
+    private static class ILogPrint {
+
+        /**
+         * 开始时间
+         */
+        private String beginTime;
+
+        /**
+         * 输入入参
+         */
+        private Object[] inputParams;
+
+        /**
+         * 输出参数
+         */
+        private Object outputParams;
+    }
+}
