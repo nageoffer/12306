6a6f91280ee70ef81b937a0ff71b0ff69180fde2
chen.ma <machen@apache.org>
2023-07-14 21:45:03 +0800

    feature: 用户分库分表物理表生成

--- services/user-service/src/test/java/org/opengoofy/index12306/biz/userservice/UserServiceApplicationTests.java
+++ services/user-service/src/test/java/org/opengoofy/index12306/biz/userservice/UserServiceApplicationTests.java
@@ -23,8 +23,27 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class UserServiceApplicationTests {
 
+    private static final String SQL = "CREATE TABLE `t_passenger_%d` (\n" +
+            "  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID',\n" +
+            "  `username` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '用户名',\n" +
+            "  `real_name` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '真实姓名',\n" +
+            "  `id_type` int(3) DEFAULT NULL COMMENT '证件类型',\n" +
+            "  `id_card` varchar(256) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '证件号码',\n" +
+            "  `discount_type` int(3) DEFAULT NULL COMMENT '优惠类型',\n" +
+            "  `phone` varchar(128) COLLATE utf8mb4_unicode_ci DEFAULT NULL COMMENT '手机号',\n" +
+            "  `create_date` datetime DEFAULT NULL COMMENT '添加日期',\n" +
+            "  `verify_status` int(3) DEFAULT NULL COMMENT '审核状态',\n" +
+            "  `create_time` datetime DEFAULT NULL COMMENT '创建时间',\n" +
+            "  `update_time` datetime DEFAULT NULL COMMENT '修改时间',\n" +
+            "  `del_flag` tinyint(1) DEFAULT NULL COMMENT '删除标识',\n" +
+            "  PRIMARY KEY (`id`)\n" +
+            ") ENGINE=InnoDB AUTO_INCREMENT=1679166254708391937 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='乘车人表';";
+
     @Test
     void contextLoads() {
+        for (int i = 0; i < 16; i++) {
+            System.out.println(String.format(SQL, i));
+        }
     }
 
 }
