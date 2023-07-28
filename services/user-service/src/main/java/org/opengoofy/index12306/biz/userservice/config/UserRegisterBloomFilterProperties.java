32be7489e37ec3b5fdbbf6ce1b443faa6ba9b7be
chen.ma <machen@apache.org>
2023-07-28 12:35:33 +0800

    feature: 布隆过滤器适配星球公用中间件

--- services/user-service/src/main/java/org/opengoofy/index12306/biz/userservice/config/UserRegisterBloomFilterProperties.java
+++ services/user-service/src/main/java/org/opengoofy/index12306/biz/userservice/config/UserRegisterBloomFilterProperties.java
@@ -34,7 +34,7 @@ public final class UserRegisterBloomFilterProperties {
     /**
      * 用户注册布隆过滤器实例名称
      */
-    private String name = " user_register_cache_penetration_bloom_filter";
+    private String name = "user_register_cache_penetration_bloom_filter";
 
     /**
      * 每个元素的预期插入量
