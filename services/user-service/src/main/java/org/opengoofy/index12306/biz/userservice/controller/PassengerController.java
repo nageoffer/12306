3fe10902fdbf792e3361c8b00f91e0dfdea65d84
chen.ma <machen@apache.org>
2023-08-02 20:51:23 +0800

    feature: 乘车人管理支持 HTTP 接口防重复提交

--- services/user-service/src/main/java/org/opengoofy/index12306/biz/userservice/controller/PassengerController.java
+++ services/user-service/src/main/java/org/opengoofy/index12306/biz/userservice/controller/PassengerController.java
@@ -24,7 +24,11 @@ import org.opengoofy.index12306.biz.userservice.dto.resp.PassengerActualRespDTO;
 import org.opengoofy.index12306.biz.userservice.dto.resp.PassengerRespDTO;
 import org.opengoofy.index12306.biz.userservice.service.PassengerService;
 import org.opengoofy.index12306.framework.starter.convention.result.Result;
+import org.opengoofy.index12306.framework.starter.idempotent.annotation.Idempotent;
+import org.opengoofy.index12306.framework.starter.idempotent.enums.IdempotentSceneEnum;
+import org.opengoofy.index12306.framework.starter.idempotent.enums.IdempotentTypeEnum;
 import org.opengoofy.index12306.framework.starter.web.Results;
+import org.opengoofy.index12306.frameworks.starter.user.core.UserContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -48,14 +52,14 @@ public class PassengerController {
      * 根据用户名查询乘车人列表
      */
     @GetMapping("/api/user-service/passenger/query")
-    public Result<List<PassengerRespDTO>> listPassengerQueryByUsername(String username) {
-        return Results.success(passengerService.listPassengerQueryByUsername(username));
+    public Result<List<PassengerRespDTO>> listPassengerQueryByUsername() {
+        return Results.success(passengerService.listPassengerQueryByUsername(UserContext.getUsername()));
     }
 
     /**
      * 根据乘车人 ID 集合查询乘车人列表
      */
-    @GetMapping("/api/user-service/passenger/actual/query/ids")
+    @GetMapping("/api/user-service/inner/passenger/actual/query/ids")
     public Result<List<PassengerActualRespDTO>> listPassengerQueryByIds(@RequestParam("username") String username, @RequestParam("ids") List<Long> ids) {
         return Results.success(passengerService.listPassengerQueryByIds(username, ids));
     }
@@ -63,6 +67,13 @@ public class PassengerController {
     /**
      * 新增乘车人
      */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在新增乘车人，请稍后再试..."
+    )
     @PostMapping("/api/user-service/passenger/save")
     public Result<Void> savePassenger(@RequestBody PassengerReqDTO requestParam) {
         passengerService.savePassenger(requestParam);
@@ -72,6 +83,13 @@ public class PassengerController {
     /**
      * 修改乘车人
      */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在修改乘车人，请稍后再试..."
+    )
     @PostMapping("/api/user-service/passenger/update")
     public Result<Void> updatePassenger(@RequestBody PassengerReqDTO requestParam) {
         passengerService.updatePassenger(requestParam);
@@ -80,7 +98,15 @@ public class PassengerController {
 
     /**
      * 移除乘车人
+     * TODO 方法命名错误，remote -> remove
      */
+    @Idempotent(
+            uniqueKeyPrefix = "index12306-user:lock_passenger-alter:",
+            key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
+            type = IdempotentTypeEnum.SPEL,
+            scene = IdempotentSceneEnum.RESTAPI,
+            message = "正在移除乘车人，请稍后再试..."
+    )
     @PostMapping("/api/user-service/passenger/remote")
     public Result<Void> removePassenger(@RequestBody PassengerRemoveReqDTO requestParam) {
         passengerService.removePassenger(requestParam);
