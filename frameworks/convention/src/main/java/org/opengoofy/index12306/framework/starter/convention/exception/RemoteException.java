f4afba196a63175c6128a9d027cec5674853e27f
chen.ma <machen@apache.org>
2023-08-02 21:19:25 +0800

    feature: 扩展远程调用异常构造函数

--- frameworks/convention/src/main/java/org/opengoofy/index12306/framework/starter/convention/exception/RemoteException.java
+++ frameworks/convention/src/main/java/org/opengoofy/index12306/framework/starter/convention/exception/RemoteException.java
@@ -17,6 +17,7 @@
 
 package org.opengoofy.index12306.framework.starter.convention.exception;
 
+import org.opengoofy.index12306.framework.starter.convention.errorcode.BaseErrorCode;
 import org.opengoofy.index12306.framework.starter.convention.errorcode.IErrorCode;
 
 /**
@@ -25,6 +26,10 @@ import org.opengoofy.index12306.framework.starter.convention.errorcode.IErrorCod
  * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
  */
 public class RemoteException extends AbstractException {
+
+    public RemoteException(String message) {
+        this(message, null, BaseErrorCode.REMOTE_ERROR);
+    }
 
     public RemoteException(String message, IErrorCode errorCode) {
         this(message, null, errorCode);
