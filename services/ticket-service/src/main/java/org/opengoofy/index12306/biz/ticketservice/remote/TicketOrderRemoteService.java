c7b71c4cefe2307520f774be598ccf7a061d2d71
chen.ma <machen@apache.org>
2023-08-16 22:56:40 +0800

    feature: 增加购票服务查询订单详情远程调用接口

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/remote/TicketOrderRemoteService.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/remote/TicketOrderRemoteService.java
@@ -19,10 +19,13 @@ package org.opengoofy.index12306.biz.ticketservice.remote;
 
 import org.opengoofy.index12306.biz.ticketservice.dto.req.CancelTicketOrderReqDTO;
 import org.opengoofy.index12306.biz.ticketservice.remote.dto.TicketOrderCreateRemoteReqDTO;
+import org.opengoofy.index12306.biz.ticketservice.remote.dto.TicketOrderDetailRespDTO;
 import org.opengoofy.index12306.framework.starter.convention.result.Result;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * 车票订单远程服务调用
@@ -31,6 +34,15 @@ import org.springframework.web.bind.annotation.RequestBody;
  */
 @FeignClient(value = "index12306-order${unique-name:}-service", url = "${aggregation.remote-url:}")
 public interface TicketOrderRemoteService {
+
+    /**
+     * 跟据订单号查询车票订单
+     *
+     * @param orderSn 列车订单号
+     * @return 列车订单记录
+     */
+    @GetMapping("/api/order-service/order/ticket/query")
+    Result<TicketOrderDetailRespDTO> queryTicketOrderByOrderSn(@RequestParam(value = "orderSn") String orderSn);
 
     /**
      * 创建车票订单
---
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/remote/dto/TicketOrderDetailRespDTO.java
@@ -0,0 +1,87 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opengoofy.index12306.biz.ticketservice.remote.dto;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * 车票订单详情返回参数
+ *
+ * @公众号：马丁玩编程，回复：加群，添加马哥微信（备注：12306）获取项目资料
+ */
+@Data
+public class TicketOrderDetailRespDTO {
+
+    /**
+     * 订单号
+     */
+    private String orderSn;
+
+    /**
+     * 列车 ID
+     */
+    private Long trainId;
+
+    /**
+     * 出发站点
+     */
+    private String departure;
+
+    /**
+     * 到达站点
+     */
+    private String arrival;
+
+    /**
+     * 乘车日期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date ridingDate;
+
+    /**
+     * 订票日期
+     */
+    @JsonFormat(pattern = "yyyy-MM-dd", timezone = "GMT+8")
+    private Date orderTime;
+
+    /**
+     * 列车车次
+     */
+    private String trainNumber;
+
+    /**
+     * 出发时间
+     */
+    @JsonFormat(pattern = "HH:mm", timezone = "GMT+8")
+    private Date departureTime;
+
+    /**
+     * 到达时间
+     */
+    @JsonFormat(pattern = "HH:mm", timezone = "GMT+8")
+    private Date arrivalTime;
+
+    /**
+     * 乘车人订单详情
+     */
+    private List<TicketOrderPassengerDetailRespDTO> passengerDetails;
+}
