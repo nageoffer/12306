05d68bef876552ccdf4cf17d3e72751bb252252c
Andy Shaw <570057644@qq.com>
2023-08-16 07:50:31 +0000

    !58 fix: 修复查询用户名是否存在时返回的布尔值 * fix: 修复查询用户名是否存在时返回的布尔值

--- services/user-service/src/main/java/org/opengoofy/index12306/biz/userservice/service/impl/UserLoginServiceImpl.java
+++ services/user-service/src/main/java/org/opengoofy/index12306/biz/userservice/service/impl/UserLoginServiceImpl.java
@@ -156,7 +156,7 @@ public class UserLoginServiceImpl implements UserLoginService {
             StringRedisTemplate instance = (StringRedisTemplate) distributedCache.getInstance();
             return instance.opsForSet().isMember(USER_REGISTER_REUSE_SHARDING + hashShardingIdx(username), username);
         }
-        return true;
+        return false;
     }
 
     @Transactional(rollbackFor = Exception.class)
