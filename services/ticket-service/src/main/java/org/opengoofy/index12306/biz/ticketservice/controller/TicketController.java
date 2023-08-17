efd9476de313613e60b940664e1bd62109d8cddc
张益豪 <onesuperman2019@outlook.com>
2023-08-17 02:48:41 +0000

    !61 fix: 优化幂等注解 keyTimeOut 10 秒参数不生效删除

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/controller/TicketController.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/controller/TicketController.java
@@ -65,7 +65,6 @@ public class TicketController {
             key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
             message = "正在执行下单流程，请稍后...",
             scene = IdempotentSceneEnum.RESTAPI,
-            keyTimeout = 10,
             type = IdempotentTypeEnum.SPEL
     )
     @PostMapping("/api/ticket-service/ticket/purchase")
@@ -82,7 +81,6 @@ public class TicketController {
             key = "T(org.opengoofy.index12306.frameworks.starter.user.core.UserContext).getUsername()",
             message = "正在执行下单流程，请稍后...",
             scene = IdempotentSceneEnum.RESTAPI,
-            keyTimeout = 10,
             type = IdempotentTypeEnum.SPEL
     )
     @PostMapping("/api/ticket-service/ticket/purchase/v2")
