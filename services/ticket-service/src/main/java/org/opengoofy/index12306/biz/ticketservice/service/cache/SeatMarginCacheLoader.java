6038e4c3f1e25c701e8e72594c046f2ea6c56b4d
chen.ma <machen@apache.org>
2023-08-18 08:31:38 +0800

    fix: 修复座位余量缓存加载业务逻辑

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/service/cache/SeatMarginCacheLoader.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/service/cache/SeatMarginCacheLoader.java
@@ -20,10 +20,13 @@ package org.opengoofy.index12306.biz.ticketservice.service.cache;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import lombok.RequiredArgsConstructor;
+import org.opengoofy.index12306.biz.ticketservice.common.enums.SeatStatusEnum;
 import org.opengoofy.index12306.biz.ticketservice.dao.entity.SeatDO;
 import org.opengoofy.index12306.biz.ticketservice.dao.entity.TrainDO;
 import org.opengoofy.index12306.biz.ticketservice.dao.mapper.SeatMapper;
 import org.opengoofy.index12306.biz.ticketservice.dao.mapper.TrainMapper;
+import org.opengoofy.index12306.biz.ticketservice.dto.domain.RouteDTO;
+import org.opengoofy.index12306.biz.ticketservice.service.TrainStationService;
 import org.opengoofy.index12306.framework.starter.cache.DistributedCache;
 import org.opengoofy.index12306.framework.starter.cache.toolkit.CacheUtil;
 import org.redisson.api.RLock;
@@ -31,14 +34,13 @@ import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
-import java.util.concurrent.TimeUnit;
 
-import static org.opengoofy.index12306.biz.ticketservice.common.constant.Index12306Constant.ADVANCE_TICKET_DAY;
-import static org.opengoofy.index12306.biz.ticketservice.common.constant.RedisKeyConstant.TRAIN_STATION_REMAINING_TICKET;
 import static org.opengoofy.index12306.biz.ticketservice.common.constant.RedisKeyConstant.LOCK_SAFE_LOAD_SEAT_MARGIN_GET;
+import static org.opengoofy.index12306.biz.ticketservice.common.constant.RedisKeyConstant.TRAIN_STATION_REMAINING_TICKET;
 
 /**
  * 座位余量缓存加载
@@ -53,9 +55,10 @@ public class SeatMarginCacheLoader {
     private final SeatMapper seatMapper;
     private final DistributedCache distributedCache;
     private final RedissonClient redissonClient;
+    private final TrainStationService trainStationService;
 
     public Map<String, String> load(String trainId, String seatType, String departure, String arrival) {
-        Map<String, String> trainStationRemainingTicket = new HashMap<>();
+        Map<String, Map<String, String>> trainStationRemainingTicketMaps = new LinkedHashMap<>();
         String keySuffix = CacheUtil.buildKey(trainId, departure, arrival);
         RLock lock = redissonClient.getLock(String.format(LOCK_SAFE_LOAD_SEAT_MARGIN_GET, keySuffix));
         lock.lock();
@@ -64,41 +67,61 @@ public class SeatMarginCacheLoader {
             Object quantityObj = stringRedisTemplate.opsForHash().get(TRAIN_STATION_REMAINING_TICKET + keySuffix, seatType);
             if (CacheUtil.isNullOrBlank(quantityObj)) {
                 TrainDO trainDO = trainMapper.selectById(trainId);
+                List<RouteDTO> routeDTOList = trainStationService.listTrainStationRoute(trainId, trainDO.getStartStation(), trainDO.getEndStation());
                 switch (trainDO.getTrainType()) {
+                    // TODO 通过已有列车类型座位枚举重构
                     case 0 -> {
-                        trainStationRemainingTicket.put("0", selectSeatMargin(trainId, 0, departure, arrival));
-                        trainStationRemainingTicket.put("1", selectSeatMargin(trainId, 1, departure, arrival));
-                        trainStationRemainingTicket.put("2", selectSeatMargin(trainId, 2, departure, arrival));
+                        for (RouteDTO each : routeDTOList) {
+                            Map<String, String> trainStationRemainingTicket = new LinkedHashMap<>();
+                            trainStationRemainingTicket.put("0", selectSeatMargin(trainId, 0, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("1", selectSeatMargin(trainId, 1, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("2", selectSeatMargin(trainId, 2, each.getStartStation(), each.getEndStation()));
+                            String actualKeySuffix = CacheUtil.buildKey(trainId, each.getStartStation(), each.getEndStation());
+                            trainStationRemainingTicketMaps.put(TRAIN_STATION_REMAINING_TICKET + actualKeySuffix, trainStationRemainingTicket);
+                        }
                     }
                     case 1 -> {
-                        trainStationRemainingTicket.put("3", selectSeatMargin(trainId, 3, departure, arrival));
-                        trainStationRemainingTicket.put("4", selectSeatMargin(trainId, 4, departure, arrival));
-                        trainStationRemainingTicket.put("5", selectSeatMargin(trainId, 5, departure, arrival));
-                        trainStationRemainingTicket.put("13", selectSeatMargin(trainId, 13, departure, arrival));
+                        for (RouteDTO each : routeDTOList) {
+                            Map<String, String> trainStationRemainingTicket = new LinkedHashMap<>();
+                            trainStationRemainingTicket.put("3", selectSeatMargin(trainId, 3, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("4", selectSeatMargin(trainId, 4, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("5", selectSeatMargin(trainId, 5, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("13", selectSeatMargin(trainId, 13, each.getStartStation(), each.getEndStation()));
+                            String actualKeySuffix = CacheUtil.buildKey(trainId, each.getStartStation(), each.getEndStation());
+                            trainStationRemainingTicketMaps.put(TRAIN_STATION_REMAINING_TICKET + actualKeySuffix, trainStationRemainingTicket);
+                        }
                     }
                     case 2 -> {
-                        trainStationRemainingTicket.put("6", selectSeatMargin(trainId, 6, departure, arrival));
-                        trainStationRemainingTicket.put("7", selectSeatMargin(trainId, 7, departure, arrival));
-                        trainStationRemainingTicket.put("8", selectSeatMargin(trainId, 8, departure, arrival));
-                        trainStationRemainingTicket.put("13", selectSeatMargin(trainId, 13, departure, arrival));
+                        for (RouteDTO each : routeDTOList) {
+                            Map<String, String> trainStationRemainingTicket = new LinkedHashMap<>();
+                            trainStationRemainingTicket.put("6", selectSeatMargin(trainId, 6, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("7", selectSeatMargin(trainId, 7, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("8", selectSeatMargin(trainId, 8, each.getStartStation(), each.getEndStation()));
+                            trainStationRemainingTicket.put("13", selectSeatMargin(trainId, 13, each.getStartStation(), each.getEndStation()));
+                            String actualKeySuffix = CacheUtil.buildKey(trainId, each.getStartStation(), each.getEndStation());
+                            trainStationRemainingTicketMaps.put(TRAIN_STATION_REMAINING_TICKET + actualKeySuffix, trainStationRemainingTicket);
+                        }
                     }
                 }
-                String buildCacheKey = TRAIN_STATION_REMAINING_TICKET + keySuffix;
-                stringRedisTemplate.opsForHash().putAll(buildCacheKey, trainStationRemainingTicket);
-                stringRedisTemplate.expire(buildCacheKey, ADVANCE_TICKET_DAY, TimeUnit.DAYS);
+                // TODO LUA 脚本执行
+                trainStationRemainingTicketMaps.forEach((cacheKey, cacheMap) -> stringRedisTemplate.opsForHash().putAll(cacheKey, cacheMap));
             }
         } finally {
             lock.unlock();
         }
-        return trainStationRemainingTicket;
+        return Optional.ofNullable(trainStationRemainingTicketMaps.get(TRAIN_STATION_REMAINING_TICKET + keySuffix))
+                .orElse(new LinkedHashMap<>());
     }
 
     private String selectSeatMargin(String trainId, Integer type, String departure, String arrival) {
         LambdaQueryWrapper<SeatDO> queryWrapper = Wrappers.lambdaQuery(SeatDO.class)
                 .eq(SeatDO::getTrainId, trainId)
                 .eq(SeatDO::getSeatType, type)
+                .eq(SeatDO::getSeatStatus, SeatStatusEnum.AVAILABLE.getCode())
                 .eq(SeatDO::getStartStation, departure)
                 .eq(SeatDO::getEndStation, arrival);
-        return Optional.ofNullable(seatMapper.selectCount(queryWrapper)).map(String::valueOf).orElse("0");
+        return Optional.ofNullable(seatMapper.selectCount(queryWrapper))
+                .map(String::valueOf)
+                .orElse("0");
     }
 }
