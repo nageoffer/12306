d32628e43cc4211ea6bebe129caa0a623bf8d984
chen.ma <machen@apache.org>
2023-08-20 18:20:43 +0800

    data: 优化初始化数据质量

--- resources/data/12306-springboot.sql
+++ resources/data/12306-springboot.sql
@@ -9,7 +9,7 @@ INSERT INTO `t_user_15` (`id`, `username`, `password`, `real_name`, `region`, `i
                          `telephone`, `mail`, `user_type`, `verify_status`, `post_code`, `address`, `deletion_time`,
                          `create_time`, `update_time`, `del_flag`)
 VALUES (1683025552364568576, 'admin', 'admin123456', '徐万里', '0', 0, 'r1yuubeRZATLkpBLkPT33CNry/L2FXqTDGTfW00JqPk=',
-        'yKZz0xLyjNb9LSCOCfJD4w==', NULL, 'i00jYS6pbWVtSXB4iAMuJ9vd6cnZ4rz4qKBAXQ9C9oU=', NULL, NULL, NULL, NULL, 0,
+        'yKZz0xLyjNb9LSCOCfJD4w==', NULL, 'i00jYS6pbWVtSXB4iAMuJ9vd6cnZ4rz4qKBAXQ9C9oU=', 0, 0, NULL, NULL, 0,
         '2023-07-23 16:05:35', '2023-07-23 16:05:35', 0);
 
 INSERT INTO `t_user_mail_14` (`id`, `username`, `mail`, `deletion_time`, `create_time`, `update_time`, `del_flag`)
