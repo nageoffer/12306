40ac790c5d207b9cb8f1775e54910182a3382081
chen.ma <machen@apache.org>
2023-08-21 23:14:08 +0800

    feature: 开发计算出发站和终点站需要扣减余票的站点（包含出发站和终点站）

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/toolkit/StationCalculateUtil.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/toolkit/StationCalculateUtil.java
@@ -20,6 +20,7 @@ package org.opengoofy.index12306.biz.ticketservice.toolkit;
 import org.opengoofy.index12306.biz.ticketservice.dto.domain.RouteDTO;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -53,5 +54,42 @@ public final class StationCalculateUtil {
             }
         }
         return routesToDeduct;
+    }
+
+    /**
+     * 计算出发站和终点站需要扣减余票的站点（包含出发站和终点站）
+     *
+     * @param stations     所有站点数据
+     * @param startStation 出发站
+     * @param endStation   终点站
+     * @return 出发站和终点站需要扣减余票的站点（包含出发站和终点站）
+     */
+    public static List<RouteDTO> takeoutStation(List<String> stations, String startStation, String endStation) {
+        List<RouteDTO> takeoutStationList = new ArrayList<>();
+        int startIndex = stations.indexOf(startStation);
+        int endIndex = stations.indexOf(endStation);
+        if (startIndex == -1 || endIndex == -1 || startIndex >= endIndex) {
+            return takeoutStationList;
+        }
+        if (startIndex != 0) {
+            for (int i = 0; i < startIndex; i++) {
+                for (int j = 1; j < stations.size() - startIndex; j++) {
+                    takeoutStationList.add(new RouteDTO(stations.get(i), stations.get(startIndex + j)));
+                }
+            }
+        }
+        for (int i = startIndex; i <= endIndex; i++) {
+            for (int j = i + 1; j < stations.size() && i < endIndex; j++) {
+                takeoutStationList.add(new RouteDTO(stations.get(i), stations.get(j)));
+            }
+        }
+        return takeoutStationList;
+    }
+
+    public static void main(String[] args) {
+        List<String> stations = Arrays.asList("北京南", "济南西", "南京南", "杭州东", "宁波");
+        String startStation = "北京南";
+        String endStation = "南京南";
+        StationCalculateUtil.takeoutStation(stations, startStation, endStation).forEach(System.out::println);
     }
 }
