f35cab755c1117e07cb5d890e86baeb737673643
卑微小曾 <2084276225@qq.com>
2023-08-22 14:44:52 +0000

    !71 optimize: 修改执行标识方式

--- frameworks/base/src/main/java/org/opengoofy/index12306/framework/starter/bases/init/ApplicationContentPostProcessor.java
+++ frameworks/base/src/main/java/org/opengoofy/index12306/framework/starter/bases/init/ApplicationContentPostProcessor.java
@@ -22,6 +22,8 @@ import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationListener;
 
+import java.util.concurrent.atomic.AtomicBoolean;
+
 /**
  * 应用初始化后置处理器，防止Spring事件被多次执行
  *
@@ -35,15 +37,13 @@ public class ApplicationContentPostProcessor implements ApplicationListener<Appl
     /**
      * 执行标识，确保Spring事件 {@link ApplicationReadyEvent} 有且执行一次
      */
-    private boolean executeOnlyOnce = true;
+    private final AtomicBoolean executeOnlyOnce = new AtomicBoolean(false);
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent event) {
-        synchronized (ApplicationContentPostProcessor.class) {
-            if (executeOnlyOnce) {
-                applicationContext.publishEvent(new ApplicationInitializingEvent(this));
-                executeOnlyOnce = false;
-            }
+        if (!executeOnlyOnce.compareAndSet(false, true)) {
+            return;
         }
+        applicationContext.publishEvent(new ApplicationInitializingEvent(this));
     }
 }
