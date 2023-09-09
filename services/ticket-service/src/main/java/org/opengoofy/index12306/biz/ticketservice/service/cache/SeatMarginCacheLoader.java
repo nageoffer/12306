6c5033edf7a30acf9dccffa84ee3763ed6a7d88c
chen.ma <machen@apache.org>
2023-09-09 14:39:39 +0800

    docs: 新增缓存带来的分布式互斥锁还有哪些优化项

--- services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/service/cache/SeatMarginCacheLoader.java
+++ services/ticket-service/src/main/java/org/opengoofy/index12306/biz/ticketservice/service/cache/SeatMarginCacheLoader.java
@@ -63,6 +63,7 @@ public class SeatMarginCacheLoader {
     public Map<String, String> load(String trainId, String seatType, String departure, String arrival) {
         Map<String, Map<String, String>> trainStationRemainingTicketMaps = new LinkedHashMap<>();
         String keySuffix = CacheUtil.buildKey(trainId, departure, arrival);
+        // 缓存带来的分布式互斥锁还有哪些优化项？详情查看：https://t.zsxq.com/12gLg7BQn
         RLock lock = redissonClient.getLock(String.format(LOCK_SAFE_LOAD_SEAT_MARGIN_GET, keySuffix));
         lock.lock();
         try {
