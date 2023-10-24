a8b95857bb17eca96b0acd521d397d3e34bb7adf
chen.ma <machen@apache.org>
2023-10-25 00:28:45 +0800

    fix: 修改前端控制台调用后端请求地址

--- console-vue/vue.config.js
+++ console-vue/vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://kz2jnh.natappfree.cc',
+        target: 'http://127.0.0.1:9000',
         changeOrigin: true,
         ws: true
       }
